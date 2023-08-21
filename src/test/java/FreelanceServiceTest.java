4195dd70fc774aa5751c5f358d6902577ab5e297
Maryana Varfolomeeva <mihralievam@icloud.com>
2023-08-21 13:05:50 +0500

    add csv-file

--- src/test/java/FreelanceServiceTest.java
+++ src/test/java/FreelanceServiceTest.java
@@ -1,15 +1,13 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.service.FreelanceService;
 
 public class FreelanceServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "3, 10000, 3000, 20000",
-            "2, 100000, 60000, 150000"
-    })
+    @CsvFileSource(files = "src/test/resources/test_data.csv")
     public void testFreelanceServiceCalculate(int expected, int income, int expenses, int threshold) {
         FreelanceService freelanceService = new FreelanceService();
         int actual = freelanceService.calculate(income, expenses, threshold);
